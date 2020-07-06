72f66941839a6db8f84a56ae6b87510c8376c122
Alper Gundogdu <mail@alpergundogdu.com>
2020-07-06 18:04:30 -0400

    Remove duplicate code

--- commands/cat.py
+++ commands/cat.py
@@ -42,29 +42,6 @@ class CatQueue():
         return self.queue.qsize()
 
 
-def fetch_cat(cat_queue, bot, update):
-    update.message.reply_photo(cat_queue.get())
-
-
-def fetch_breeds(bot, update):
-    update.message.reply_text(
-        'Breeds: ' + ', '.join(['/' + name for name in sorted(NAMES_IDS.keys())]))
-
-
-def add_command(dispatcher, cat_queue, commands):
-    dispatcher.add_handler(CommandHandler(
-        commands, partial(fetch_cat, cat_queue)))
-
-
-def install_telegram(dispatcher):
-    dispatcher.add_handler(CommandHandler(
-        ['start', 'cat'], partial(fetch_cat, CatQueue(capacity=50))))
-    for name, breed_id in NAMES_IDS.items():
-        dispatcher.add_handler(CommandHandler(name, partial(
-            fetch_cat, CatQueue(breeds=[breed_id], capacity=5))))
-    dispatcher.add_handler(CommandHandler('breeds', fetch_breeds))
-
-
 if __name__ == "__main__":
     cat_queue = CatQueue(breeds=['tvan'])
     while True:
